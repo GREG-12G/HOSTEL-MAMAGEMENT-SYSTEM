59c8561f99cf7375650ad952105f03460e3f9558
GREG <gregmaina618@gmail.com>
2026-09-09 12:53:28 +0300

    Add function to register new students
    Implement add_student function to add student details.

---
+++ students.py
@@ -0,0 +1,25 @@
+from logging_config import logger
+students = []
+def add_student(): 
+    print("\n--- Add Student---")
+
+    name = input("Entr Student Name:")
+    registraton_number = input("Enter Registration Number")
+    phone_number = input("Enter Phone Number")
+    course = input("Enter course")
+
+    student = {
+         "name":name'
+         "registration_number":registration_number,
+         "phone_number":phone number,
+         "course":course,
+    }
+    students.append(student)
+    logger.info(
+        f"Student added:{name},Registration:{registration_number})
+    print("Student Added Succesfully !")
+
+    def view_students():
+                     
+                   )
+  
