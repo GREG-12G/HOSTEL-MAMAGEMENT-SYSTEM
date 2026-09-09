e7ec58840e6c37e7fb90e82f66f8787f2dcdacd5
GREG <gregmaina618@gmail.com>
2026-09-09 11:40:22 +0300

    Implement hostel management system menu
    Added a menu for hostel management system with options.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+# HOSTEL-MAMAGEMENT-SYSTEM
+A project aimed at designing a hostel management
 
+print("Hostel management system")
+
+print("1. Add hostel")
+print("2. Add Student")
+print("3. Record Hostel Payment")
+print("4. View Hostel")
+print("5. View Student")
+print("6. Exit")
+
+choice=input("Enter Choice :")
+if choice =="1":
+    print("Add Hostel Selected")
+elif choice == "2":
+    print("Add Student Selected")
+elif choice == "3":
+    print("Record Hostel Payment Selected")    
+elif choice == "4":
+    print("View Hostel Helected")
+elif choice == "5":
+    print("View Student Selected")
+elif choice == "6":
+    print("Goodbye") 
+els:
+    print("Invalid Choice")
