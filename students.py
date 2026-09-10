9d43a1dc1183828aa0ba04f45247209eff09ed96
GREG <gregmaina618@gmail.com>
2026-09-10 16:24:58 +0300

    Update students.py

--- students.py
+++ students.py
@@ -1,24 +1,35 @@
-from logging_config import logger
-
-students = []
+  from logging_config import logger
+from database import get_connection
 
 
 def add_student():
     print("\n--- Add Student ---")
 
-    name = input("Enter Student Name: ")
-    registration_number = input("Enter Registration Number: ")
-    phone_number = input("Enter Phone Number: ")
-    course = input("Enter Course: ")
+    name = input("Enter Student Name: ").strip()
+    registration_number = input("Enter Registration Number: ").strip()
+    phone_number = input("Enter Phone Number: ").strip()
+    course = input("Enter Course: ").strip()
 
-    student = {
-        "name": name,
-        "registration_number": registration_number,
-        "phone_number": phone_number,
-        "course": course,
-    }
+    connection = get_connection()
+    cursor = connection.cursor()
 
-    students.append(student)
+    cursor.execute("""
+        INSERT INTO students (
+            name,
+            registration_number,
+            phone,
+            course
+        )
+        VALUES (?, ?, ?, ?)
+    """, (
+        name,
+        registration_number,
+        phone_number,
+        course
+    ))
+
+    connection.commit()
+    connection.close()
 
     logger.info(
         f"Student added: {name}, Registration: {registration_number}"
@@ -30,14 +41,35 @@ def add_student():
 def view_students():
     print("\n--- Students List ---")
 
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT
+            id,
+            name,
+            registration_number,
+            phone,
+            course
+        FROM students
+        ORDER BY id
+    """)
+
+    students = cursor.fetchall()
+
+    connection.close()
+
     if not students:
         print("No students registered.")
         return
 
     for student in students:
-        print(f"Name: {student['name']}")
-        print(f"Registration Number: {student['registration_number']}")
-        print(f"Phone Number: {student['phone_number']}")
-        print(f"Course: {student['course']}")
+        student_id, name, registration_number, phone, course = student
+
+        print(f"Student ID: {student_id}")
+        print(f"Name: {name}")
+        print(f"Registration Number: {registration_number}")
+        print(f"Phone Number: {phone}")
+        print(f"Course: {course}")
         print("-" * 30)
-        
+
