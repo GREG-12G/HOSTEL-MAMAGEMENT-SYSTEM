665f73b1d027987074706fd737576ad0660c31a6
GREG <gregmaina618@gmail.com>
2026-09-10 11:56:58 +0300

    Refactor main.py to improve menu handling

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 # HOSTEL-MAMAGEMENT-SYSTEM
 from logging_config import logger
+from students import add_student, view_students
+
 
 logger.info("Hostel Management Started")
-print("Welcome To Embu Hostels Management system ")
+
+print("Welcome To Embu Hostels Management System")
+
 logger.info("System Is Running")
 
 
+while True:
+    print("\n1. Add Hostel")
+    print("2. Add Student")
+    print("3. Record Hostel Payment")
+    print("4. View Hostel")
+    print("5. View Student")
+    print("6. Exit")
 
-print("1. Add hostel")
-print("2. Add Student")
-print("3. Record Hostel Payment")
-print("4. View Hostel")
-print("5. View Student")
-print("6. Exit")
+    choice = input("Enter Choice: ")
 
-choice=input("Enter Choice :")
-if choice =="1":
-    print("Add Hostel Selected")
-elif choice == "2":
-    print("Add Student Selected")
-elif choice == "3":
-    print("Record Hostel Payment Selected")    
-elif choice == "4":
-    print("View Hostel Helected")
-elif choice == "5":
-    print("View Student Selected")
-elif choice == "6":
-    print("Goodbye") 
-els:
-    print("Invalid Choice")
+    if choice == "1":
+        print("Add Hostel Selected")
+
+    elif choice == "2":
+        add_student()
+
+    elif choice == "3":
+        print("Record Hostel Payment Selected")
+
+    elif choice == "4":
+        print("View Hostel Selected")
+
+    elif choice == "5":
+        view_students()
+
+    elif choice == "6":
+        print("Goodbye!")
+        logger.info("System Stopped")
+        break
+
+    else:
+        print("Invalid Choice")
+        
