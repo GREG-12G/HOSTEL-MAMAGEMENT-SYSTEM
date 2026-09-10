234390b35e18158a837a35c631495f67bb2bf74b
GREG <gregmaina618@gmail.com>
2026-09-10 15:31:11 +0300

    Fix import typo and update hostel management options

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # HOSTEL-MAMAGEMENT-SYSTEM
 from logging_config import logger
 from students import add_student, view_students
-
+from hostels impoprt add_hostel, view_hostels
 
 logger.info("Hostel Management Started")
 
@@ -20,20 +20,21 @@ while True:
 
     choice = input("Enter Choice: ")
 
-    if choice == "1":
-        print("Add Hostel Selected")
+   if choice == "1":
+        add_hostel()
 
     elif choice == "2":
         add_student()
-
+ 
     elif choice == "3":
         print("Record Hostel Payment Selected")
-
+        
     elif choice == "4":
-        print("View Hostel Selected")
+        view_hostels()
 
     elif choice == "5":
         view_students()
+    
 
     elif choice == "6":
         print("Goodbye!")
